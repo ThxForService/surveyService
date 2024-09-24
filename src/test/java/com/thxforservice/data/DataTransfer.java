f4ffc4547197666c71523f3a89ea548e754d9abe
irine3355 <irine3355@gmail.com>
2024-09-25 07:29:43 +0900

    3

--- src/test/java/com/thxforservice/data/DataTransfer.java
+++ src/test/java/com/thxforservice/data/DataTransfer.java
@@ -6,8 +6,12 @@ import com.thxforservice.survey.repositories.QuestionRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
 import java.io.File;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 @SpringBootTest
@@ -16,22 +20,29 @@ public class DataTransfer {
     @Autowired
     private QuestionRepository repository;
 
+    @Autowired
+    private ResourceLoader resourceLoader;  // 리소스 로더 주입
+
     @Test
     void tranfer() throws Exception {
-        Scanner sc = new Scanner(new File("D:/data/survey.txt"));
+        Resource resource = resourceLoader.getResource("classpath:static/files/survey.txt");
         //int EVASION = 0, STRESS = 0, INTERNET_ADDICTION = 0, EATING_DISORDER = 0;
-        while(sc.hasNextLine()) {
-            String line = sc.nextLine();
-            String[] data = line.split("\\|\\|");
-            String type = data[0];
-            String q = data[1];
+        try (InputStream inputStream = resource.getInputStream();
+             Scanner sc = new Scanner(inputStream, StandardCharsets.UTF_8)) {
 
-            Question item = Question.builder()
-                    .questionText(q)
-                    .testType(SurveyType.valueOf(type))
-                    .build();
+            while(sc.hasNextLine()) {
+                String line = sc.nextLine();
+                String[] data = line.split("\\|\\|");
+                String type = data[0];
+                String q = data[1];
 
-            repository.saveAndFlush(item);
+                Question item = Question.builder()
+                        .questionText(q)
+                        .testType(SurveyType.valueOf(type))
+                        .build();
+
+                repository.saveAndFlush(item);
+            }
         }
     }
 }
